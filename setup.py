f865e9b2443b9e2453d6aa2b24b20b2baa6fc578
btsdev <btsdevman@gmail.com>
2023-06-21 08:56:45 -0500

    Changelog update.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
   name='global_hotkeys',
-  version='0.1.5',
+  version='0.1.6',
   description='',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
   long_description_content_type='text/markdown',
